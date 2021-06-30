ba52e3988c49604b35e9d056937c21594a48434d
andresmonestel1998 <amonestel2011@hotmail.com>
2021-06-29 21:14:33 -0600

    Add project files.

---
+++ Entidades/Entidades/cl_Ent_Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Entidades
+{
+    public class cl_Ent_Cliente
+    {
+        #region variables
+            private string nombre, cedula, correo, telefono;
+
+        #endregion
+
+        #region constructores
+            public string Nombre { get => nombre; set => nombre = value; }
+            public string Cedula { get => cedula; set => cedula = value; }
+            public string Correo { get => correo; set => correo = value; }
+            public string Telefono { get => telefono; set => telefono = value; }
+        #endregion
+
+
+    }
+}
