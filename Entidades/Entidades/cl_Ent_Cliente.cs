f1dc0dbddde2d88059167c2b98c06784bee63e2a
andresmonestel1998 <amonestel2011@hotmail.com>
2021-06-30 20:04:32 -0600

    actualizado entidades

--- Entidades/Entidades/cl_Ent_Cliente.cs
+++ Entidades/Entidades/cl_Ent_Cliente.cs
@@ -5,14 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Entidades.Entidades
-{
-    public class cl_Ent_Cliente
-    {
-        #region variables
-            private string nombre, cedula, correo, telefono;
+            //hola
+            //Saludos
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        //hola
+        #endregion
+        //asdasdasda
 
         #endregion
-
+        #endregion
+            private string nombre, cedula, correo, telefono#endregion
         #region constructores
             public string Nombre { get => nombre; set => nombre = value; }
             public string Cedula { get => cedula; set => cedula = value; }
