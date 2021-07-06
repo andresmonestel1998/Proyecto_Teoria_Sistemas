f457430d1490f61729c427d2bd3179519959eaa0
andresmonestel1998 <amonestel2011@hotmail.com>
2021-07-05 22:31:05 -0600

    actualizado

--- Datos/DatosUsuario/cl_dat_Rol.cs
+++ Datos/DatosUsuario/cl_dat_Rol.cs
@@ -15,6 +15,7 @@ namespace Datos.DatosUsuario
 
         public DataTable cargarComboBoxRol()
         {
+            
             SqlCommand Comando = new SqlCommand();
 
             Comando.CommandType = CommandType.StoredProcedure;
