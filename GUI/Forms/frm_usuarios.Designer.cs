fb09021166fcc01b85775d5ef75d6db644b96c7d
andresmonestel1998 <amonestel2011@hotmail.com>
2021-07-07 23:54:28 -0600

    actualizado

--- GUI/Forms/frm_usuarios.Designer.cs
+++ GUI/Forms/frm_usuarios.Designer.cs
@@ -83,10 +83,14 @@ namespace GUI.Forms
             this.label55 = new System.Windows.Forms.Label();
             this.label56 = new System.Windows.Forms.Label();
             this.label57 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.tbControlUsuarios.SuspendLayout();
             this.tabCancelar.SuspendLayout();
             this.tabAparcarCliente.SuspendLayout();
@@ -163,6 +167,8 @@ namespace GUI.Forms
             // 
             // tabAparcarCliente
             // 
+            this.tabAparcarCliente.Controls.Add(this.comboBox1);
+            this.tabAparcarCliente.Controls.Add(this.label7);
             this.tabAparcarCliente.Controls.Add(this.label5);
             this.tabAparcarCliente.Controls.Add(this.label6);
             this.tabAparcarCliente.Controls.Add(this.gBoxClientes);
@@ -409,8 +415,10 @@ namespace GUI.Forms
             // 
             // tabAparcarInvitado
             // 
-            this.tabAparcarInvitado.Controls.Add(this.label4);
+            this.tabAparcarInvitado.Controls.Add(this.comboBox2);
             this.tabAparcarInvitado.Controls.Add(this.label3);
+            this.tabAparcarInvitado.Controls.Add(this.label4);
+            this.tabAparcarInvitado.Controls.Add(this.label8);
             this.tabAparcarInvitado.Controls.Add(this.gBoxInvitados);
             this.tabAparcarInvitado.Location = new System.Drawing.Point(4, 32);
             this.tabAparcarInvitado.Name = "tabAparcarInvitado";
@@ -443,7 +451,6 @@ namespace GUI.Forms
             this.gBoxInvitados.Controls.Add(this.label55);
             this.gBoxInvitados.Controls.Add(this.label56);
             this.gBoxInvitados.Controls.Add(this.label57);
-            this.gBoxInvitados.Enabled = false;
             this.gBoxInvitados.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gBoxInvitados.Location = new System.Drawing.Point(3, 0);
             this.gBoxInvitados.Name = "gBoxInvitados";
@@ -631,41 +638,75 @@ namespace GUI.Forms
             this.label57.TabIndex = 1;
             this.label57.Text = "Nombre del cliente:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(777, 7);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(416, 23);
-            this.label3.TabIndex = 22;
-            this.label3.Text = "Cantidad de espacios disponibles en el parqueo:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(928, 93);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 23);
-            this.label4.TabIndex = 23;
-            this.label4.Text = "Cantidad";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(894, 101);
+            this.label5.Location = new System.Drawing.Point(755, 222);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 23);
+            this.label5.Size = new System.Drawing.Size(30, 23);
             this.label5.TabIndex = 25;
-            this.label5.Text = "Cantidad";
+            this.label5.Text = "55";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(743, 15);
+            this.label6.Location = new System.Drawing.Point(755, 181);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(416, 23);
             this.label6.TabIndex = 24;
             this.label6.Text = "Cantidad de espacios disponibles en el parqueo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(755, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(187, 23);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Seleccionar parqueo:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(759, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(360, 31);
+            this.comboBox1.TabIndex = 27;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(785, 86);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(360, 31);
+            this.comboBox2.TabIndex = 31;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(781, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 23);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Seleccionar parqueo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(781, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 23);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "55";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(781, 178);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(416, 23);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Cantidad de espacios disponibles en el parqueo:";
             // 
             // frm_usuarios
             // 
@@ -749,7 +790,11 @@ namespace GUI.Forms
         private System.Windows.Forms.Label label57;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
     }
 }
